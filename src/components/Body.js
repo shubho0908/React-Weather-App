a122d7ee37c9812afa0e250294e4946854a71d5b
Shubhojeet Bera <shubhobera98@gmail.com>
2023-01-24 15:45:40 +0530

    bugs fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,10 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function Body(props) {
-
-    const temp = props.temp.toString().slice(0, 4);
-    const feels = props.feels.toString().slice(0, 4);
+  const temp = props.temp.toString().slice(0, 4);
+  const feels = props.feels.toString().slice(0, 4);
 
   return (
-
     <>
       <div className="container ">
         <div class="card my-5 justify-content-center text-center">
@@ -14,7 +12,7 @@ export default function Body(props) {
           <div class="card-body">
             <h5 class="card-title">Temp: {temp}°C</h5>
             <h5 class="card-title">Feels Like: {feels}°C</h5>
-
+            <h5 class="card-title">Humidity: {props.humidity}%</h5>
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,43 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import Body from './Body'
 
 export default function Navbar(props) {
     const [search, setsearch] = useState(0)
     const [city, setcity] = useState("")
     const [temp, settemp] = useState(0)
+    const [humidity, sethumidity] = useState(0)
     const [feels, setfeels] = useState(0)
     const [cityName, setcityName] = useState("")
+    const [alert, setalert] = useState(false)
 
     const handleChange=(e)=>{
         setcity(e.target.value)
     }
 
-    const fetchData = async()=>{
+    const fetchData = async(e)=>{
+      e.preventDefault();
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${props.apiKey}`
         let data = await fetch(url)
         let parseData = await data.json()
+        if(parseData.cod === "404"){
+          setalert(true)
+          return;
+      }
+      else{
         setcityName(parseData.name)
         settemp(parseData.main.temp)
         setfeels(parseData.main.feels_like)
+        sethumidity(parseData.main.humidity)
         setsearch(search+1)
         console.log(parseData);
+      }
     }
+
+    useEffect(() => {
+      setTimeout(() => {
+          setalert(false)
+      }, 4000);
+  }, [alert])
 
 
   return (
@@ -41,7 +57,17 @@ export default function Navbar(props) {
   </div>
 </nav>
 
-{search>0?(<Body cityName = {cityName} temp={temp-272.15} city = {city} feels={feels-272.15}/>):<p className='justify-content-center text-center my-5' style={{fontSize:"25px"}}>Nothing to show here!</p>}
+{alert ? (
+        <div class="alert alert-danger" role="alert">
+          <strong>Error!</strong> Wrong Input Data, enter city name only.
+        </div>
+      ) : (
+        ""
+      )}
+
+{search>0?(<Body humidity={humidity} cityName = {cityName} temp={temp-272.15} city = {city} feels={feels-272.15}/>):<p className='justify-content-center text-center my-5' style={{fontSize:"25px"}}>Nothing to show here!</p>}
+  
+
 
   </>
   )
