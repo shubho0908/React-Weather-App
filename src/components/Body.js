a307d0b411420899034eee531b0800004e0c864b
Shubhojeet Bera <shubhobera98@gmail.com>
2023-01-24 18:41:48 +0530

    updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Temp from './temp.png'
+import Feels from './feels.png'
+import Humidity from './humidity.png'
 
 export default function Body(props) {
   const temp = props.temp.toString().slice(0, 4);
@@ -7,13 +9,21 @@ export default function Body(props) {
 
   return (
     <>
-      <div className="container ">
-        <div className="card my-5 justify-content-center text-center">
-          <h5 className="card-header">{props.cityName}</h5>
+      <div className="container " >
+        <div className="card my-5 justify-content-center text-center" style={{border:"3px solid rgb(0 60 255 / 50%)"}}>
+          <h5 style={{fontSize:"35px"}} className="card-header">{props.cityName}</h5>
+          <div className="card-body" >
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Temp} alt="Temperature" /> Temp: {temp}°C</h5>
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Feels} alt="Feels Like" /> Feels Like: {feels}°C</h5>
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Humidity} alt="Humidity" /> Humidity: {props.humidity}%</h5>
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Humidity} alt="wind" /> Wind: {props.wind} km/h</h5>
+          </div>
+        </div>
+        <div className="card my-5 justify-content-center text-center" style={{border:"3px solid rgb(0 60 255 / 50%)"}}>
           <div className="card-body">
-            <h5 className="card-title"><img src={Temp} alt="Temperature" /> Temp: {temp}°C</h5>
-            <h5 className="card-title">Feels Like: {feels}°C</h5>
-            <h5 className="card-title">Humidity: {props.humidity}%</h5>
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Temp} alt="Temperature" /> Temp: {temp}°C</h5>
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Feels} alt="Feels Like" /> Feels Like: {feels}°C</h5>
+            <h5 style={{fontSize:"25px"}} className="card-title"><img style={{height:"50px"}} src={Humidity} alt="Humidity" /> Humidity: {props.humidity}%</h5>
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,13 @@ import React, {useState,useEffect} from 'react'
 import Body from './Body'
 import animation from './animation.gif'
 
+
 export default function Navbar(props) {
     const [search, setsearch] = useState(0)
     const [city, setcity] = useState("")
     const [temp, settemp] = useState(0)
     const [humidity, sethumidity] = useState(0)
+    const [wind, setwind] = useState(0)
     const [feels, setfeels] = useState(0)
     const [cityName, setcityName] = useState("")
     const [alert, setalert] = useState(false)
@@ -28,6 +30,7 @@ export default function Navbar(props) {
         setcityName(parseData.name)
         settemp(parseData.main.temp)
         setfeels(parseData.main.feels_like)
+        setwind(parseData.wind.speed)
         sethumidity(parseData.main.humidity)
         setsearch(search+1)
         console.log(parseData);
@@ -42,7 +45,7 @@ export default function Navbar(props) {
 
   return (
   <>
-   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+   <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{borderRadius:"0px", opacity:"90%"}}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">Weather App</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -51,7 +54,7 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse justify-content-end my-2" id="navbarSupportedContent">
       <form className="d-flex " role="search">
         <input value={city} onChange={handleChange} className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button onClick={fetchData} className="btn btn-outline-success mx-1" type="submit">Search</button>
+        <button onClick={fetchData} className="btn btn-success mx-1" type="submit">Search</button>
       </form>
     </div>
   </div>
@@ -65,7 +68,7 @@ export default function Navbar(props) {
         ""
       )}
 
-{search>0?(<Body humidity={humidity} cityName = {cityName} temp={temp-272.15} city = {city} feels={feels-272.15}/>):<p className='justify-content-center text-center ' style={{fontSize:"25px", display:"flex",justifyContent:"center",alignItems:"center", flexFlow:"column"}}><img id='nothing' style={{height:"350px", width:"350px", textAlign:"center", }} src={animation} alt="" />Nothing to show here!</p>}
+{search>0?(<Body wind={wind} humidity={humidity} cityName = {cityName} temp={temp-272.15} city = {city} feels={feels-272.15}/>):<p className='justify-content-center text-center ' style={{fontSize:"25px", display:"flex",justifyContent:"center",alignItems:"center", flexFlow:"column"}}><img id='nothing' style={{height:"350px", width:"350px", textAlign:"center", }} src={animation} alt="" />Nothing to show here!</p>}
 
   </>
   )
